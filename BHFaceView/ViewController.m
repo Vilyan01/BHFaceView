ccdfe257488995c4724347a30892ab1a28221b30
Brian Heller <bheller2010@live.com>
2017-01-27 11:32:52 -0800

    Refactored the preview a bit to be more efficient.

--- BHFaceView/ViewController.m
+++ BHFaceView/ViewController.m
@@ -7,32 +7,53 @@
 //
 
 #import "ViewController.h"
+#import "AppDelegate.h"
 #import <AVFoundation/AVFoundation.h>
 
 @interface ViewController ()
 // Render a circle to this image view that will circle the face in the video.
 @property (weak, nonatomic) IBOutlet UIImageView *overlayView;
+
+// AVFoundation objects we will need to use to capture images to find faces in.
+@property (strong, nonatomic) AVCaptureSession *session;
+@property (strong, nonatomic) AVCaptureVideoPreviewLayer *layer;
+
+@property (weak, nonatomic) BHFaceDetector *detector;
 @end
 
 @implementation ViewController
 
+- (BHFaceDetector *)detector {
+    return [((AppDelegate*)[[UIApplication sharedApplication] delegate]) faceDetector];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    // Set up the camera.
+    [self initCamera];
 }
 
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
-    // Set up the camera
-    [self initCamera];
+    // Set up the preview
+    [self initPreviewLayer];
+    
+    // Start the preview
+    [_session startRunning];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
 }
 
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    [_session stopRunning];
+}
+
 - (void)initCamera {
     // Create an AVCaptureSession
-    AVCaptureSession *session = [AVCaptureSession new];
+    _session = [AVCaptureSession new];
     
     // Use the discovery session to find the front camera on the device.
     AVCaptureDeviceDiscoverySession *discovery = [AVCaptureDeviceDiscoverySession discoverySessionWithDeviceTypes:@[AVCaptureDeviceTypeBuiltInWideAngleCamera] mediaType:AVMediaTypeVideo position:AVCaptureDevicePositionFront];
@@ -57,17 +78,18 @@
     }
     
     // If we have a valid input, add it to the session.
-    if([session canAddInput:deviceInput]) {
-        [session addInput:deviceInput];
+    if([_session canAddInput:deviceInput]) {
+        [_session addInput:deviceInput];
     }
-    
-    // Create a layer to display the video preview on and add it to our view.
-    AVCaptureVideoPreviewLayer *layer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:session];
-    layer.frame = self.view.frame;
-    [self.view.layer addSublayer:layer];
-    
-    // Start the session.
-    [session startRunning];
+}
+
+- (void)initPreviewLayer {
+    if(!_layer) {
+        // Create a layer to display the video preview on and add it to our view.
+        _layer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:_session];
+        _layer.frame = self.view.frame;
+        [self.view.layer addSublayer:_layer];
+    }
 }
 
 
